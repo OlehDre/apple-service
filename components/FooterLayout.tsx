edb028b53c3891bf16d3eec2669e3ffbd8fd0b37
OlehDre <DremliuhaOO@gmail.com>
2022-09-10 01:57:46 +0300

    Fixed footer layout

--- components/FooterLayout.tsx
+++ components/FooterLayout.tsx
@@ -5,33 +5,33 @@ export function Footer(){
     return(
         <>
             <footer>
+                <Link href={'/'}><a className="logo">Apple Service</a></Link>
                 <nav>
-                    <h2 className="logo">Apple Service</h2>
-                    <Link href={'/'}><a>Home</a></Link>
-                    <Link href={'/about'}><a>About</a></Link>
-                    <Link href={'/posts'}><a>Posts</a></Link>
+                    <Link href={'/'}><a>Главная</a></Link>
+                    <Link href={'/posts'}><a>Каталог</a></Link>
+                    <Link href={'/about'}><a>Услуги</a></Link>
+                    <Link href={'/contacts'}><a>Контакты</a></Link>
                 </nav>
             </footer>
 
             <style jsx>{`
                 footer{
-                    padding: 50px;
-                    height: 60px;
-                    background: #111;
-                }
-                nav{
+                    background: #000;
+                    align-items: center;
+                    height: 150px;
                     display: flex;
                     justify-content: space-around;
-                    align-items: center;
                 }
-
                 .logo{
+                    font-size: 1.6em;
                     color: #fff;
+                    text-decoration: none;
                 }
 
                 nav a{
                     color: #fff;
                     text-decoration: none;
+                    margin: 0 20px;
                 }
             `}</style>
         </>
