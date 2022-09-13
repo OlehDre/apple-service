9762c4d09b1f3bd58e1eff3ed7d39669b0011bed
OlehDre <DremliuhaOO@gmail.com>
2022-09-13 04:12:44 +0300

    Fix

--- components/NavLayout.tsx
+++ components/NavLayout.tsx
@@ -1,3 +1,11 @@
+
+const navigation = [
+    {id: 1, title: 'Home', path: '/'},
+    {id: 2, title: 'Catalog', path: '/posts'},
+    {id: 3, title: 'Services', path: '/about'},
+    {id: 4, title: 'Contacts', path: '/contacts'}
+];
+
 export function NavLayouts(){
 
     return(
